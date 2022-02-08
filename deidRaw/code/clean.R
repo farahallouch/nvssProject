09eccbed1513899bf18563583b8ec4f081082b62
Farah Allouch <farahmallouch@gmail.com>
2022-02-08 15:51:43 -0600

    subsetting to women with GH or eclampsia for easier management of data and memory

--- deidRaw/code/clean.R
+++ deidRaw/code/clean.R
@@ -129,7 +129,7 @@ natl2020 <- natl2020 %>%
          ab_seiz = AB_SEIZ)
 
 natl2016_2020 <- rbind(natl2016, natl2017, natl2018, natl2019, natl2020) # missing RF_PPTERM, ME_PRES AND ME_ROUT
-save(natl2016_2020, file = "deidRaw/output/natl2016_2017.Rdata")
+save(natl2016_2020, file = "deidRaw/output/natl2016_2020.Rdata")
 
 rm(natl2016)
 rm(natl2017)
@@ -137,52 +137,112 @@ rm(natl2018)
 rm(natl2019)
 rm(natl2020)
 
+natl2016_2020_gh <- natl2016_2020 %>% 
+  filter(rf_ghype == "Y" |
+           rf_ehype == "Y")
 
-# table(natl2016_2017$dob_yy, useNA = "always")
-# table(natl2016_2017$bfacil3, useNA = "always")
-# summary(natl2016_2017$mager)
-# table(natl2016_2017$mbstate_rec, useNA = "always") 
-# table(natl2016_2017$mracehisp, useNA = "always") # 8 to NA
-# table(natl2016_2017$dmar, useNA = "always") 
-# table(natl2016_2017$meduc, useNA = "always") # 9 to NA
-# summary(natl2016_2017$priorlive) # 99 to NA
-# summary(natl2016_2017$priordead) # 99 to NA
-# summary(natl2016_2017$priorterm) # 99 to NA
-# summary(natl2016_2017$tbo_rec) # 9 to NA
-# summary(natl2016_2017$precare) # 99 to NA
-# summary(natl2016_2017$previs) # 99 to NA
-# table(natl2016_2017$wic, useNA = "always") # U to NA ; character to numeric 
-# summary(natl2016_2017$cig_0) # 99 to NA
-# summary(natl2016_2017$cig_1) # 99 to NA
-# summary(natl2016_2017$cig_2) # 99 to NA
-# summary(natl2016_2017$cig_3) # 99 to NA
-# summary(natl2016_2017$bmi) # 99.90 to NA
-# table(natl2016_2017$rf_pdiab, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$rf_gdiab, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$rf_phype, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$rf_ghype, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$rf_ehype, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$rf_ppterm, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ob_ecvs, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ob_ecvf, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ld_indl, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ld_augm, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ld_ster, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ld_antb, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ld_chor, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ld_anes, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$me_pres, useNA = "always") # 9 to NA
-# table(natl2016_2017$me_rout, useNA = "always") # 9 to NA
-# table(natl2016_2017$mm_mtr, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$mm_rupt, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$mm_uhyst, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$mm_aicu, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$pay_rec, useNA = "always") # 9 to NA
-# summary(natl2016_2017$apgar5) # 99 to NA
-# table(natl2016_2017$sex, useNA = "always")
-# table(natl2016_2017$oegest_r10, useNA = "always") # 99 to NA
-# summary(natl2016_2017$dbwt) # 9999 to NA
-# table(natl2016_2017$ab_aven6, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$ab_seiz, useNA = "always") # U to NA ; character to numeric 
-# table(natl2016_2017$f_ab_vent6, useNA = "always") # probs not needed
-# table(natl2016_2017$f_ab_seiz, useNA = "always") # probs not needed
+rm(natl2016_2020)
+save(natl2016_2020_gh, file = "deidRaw/output/natl2016_2020.Rdata")
+
+natl2016_2020_gh <- natl2016_2020_gh %>% 
+  rename(year = dob_yy,
+         facility = bfacil3,
+         mom_age = mager,
+         mom_nativity = mbstate_rec,
+         mom_race_ethnicity = mracehisp,
+         mom_marital_status = dmar,
+         mom_educ = meduc,
+         prior_births_living = priorlive,
+         prior_births_dead = priordead,
+         prior_terminations = priorterm,
+         total_birth_order = tbo_rec,
+         month_prenatal_start = precare,
+         nb_prenatal_visits = previs,
+         nb_cig_before_preg = cig_0,
+         nb_cig_first_trimester = cig_1,
+         nb_cig_second_trimester = cig_2,
+         nb_cig_third_trimester = cig_3,
+         pre_preg_diabetes = rf_pdiab,
+         gest_diabetes = rf_gdiab,
+         pre_preg_htn = rf_phype,
+         gest_htn = rf_ghype,
+         eclampsia = rf_ehype,
+         success_external_cephalic = ob_ecvs,
+         fail_external_cephalic = ob_ecvf,
+         labor_induction = ld_indl,
+         labor_augmentation = ld_augm,
+         labor_steroids = ld_ster,
+         labor_antibiotics = ld_antb,
+         labor_chorioamniotitis = ld_chor,
+         labor_anesthesia = ld_anes,
+         maternal_transfusion = mm_mtr,
+         ruptured_uterus = mm_rupt,
+         unplanned_hysterectomy = mm_uhyst,
+         maternal_icu = mm_aicu,
+         payer = pay_rec,
+         five_min_apgar = apgar5,
+         baby_sex = sex,
+         gest_age = oegest_r10,
+         birthweight = dbwt,
+         assisted_ventilation_six_hr = ab_aven6,
+         baby_seizures = ab_seiz) %>% 
+  filter(!is.na(year)) %>% 
+  mutate(facility = ifelse(facility == 3, NA, facility),
+         mom_nativity = ifelse(mom_nativity == 3, NA, mom_nativity),
+         mom_race_ethnicity = ifelse(mom_race_ethnicity == 8, NA, mom_race_ethnicity),
+         mom_educ = ifelse(mom_educ == 9, NA, mom_educ),
+         prior_births_living = as.numeric(prior_births_living),
+         prior_births_living = ifelse(prior_births_living == 99, NA, prior_births_living),
+         prior_births_dead = as.numeric(prior_births_dead),
+         prior_births_dead = ifelse(prior_births_dead == 99, NA, prior_births_dead),
+         prior_terminations = as.numeric(prior_terminations),
+         prior_terminations = ifelse(prior_terminations == 99, NA, prior_terminations),
+         total_birth_order = ifelse(total_birth_order == 9, NA, total_birth_order),
+         nb_prenatal_visits = as.numeric(nb_prenatal_visits),
+         nb_prenatal_visits = ifelse(nb_prenatal_visits == 99, NA, nb_prenatal_visits),
+         )
+  
+
+table(natl2016_2020_gh$year, useNA = "always") # 57 NA
+table(natl2016_2020_gh$facility, useNA = "always") # 3 to NA + 57 NA
+summary(natl2016_2020_gh$mom_age) # 57 NA
+table(natl2016_2020_gh$mom_nativity, useNA = "always") # 3 to NA
+table(natl2016_2020_gh$mom_race_ethnicity, useNA = "always") # 8 to NA
+table(natl2016_2020_gh$mom_marital_status, useNA = "always")
+table(natl2016_2020_gh$mom_educ, useNA = "always") # 9 to NA
+summary(natl2016_2020_gh$prior_births_living) # 99 to NA; character to numeric
+summary(natl2016_2020_gh$prior_births_dead) # 99 to NA; character to numeric
+summary(natl2016_2020_gh$prior_terminations) # 99 to NA; character to numeric
+summary(natl2016_2020$total_birth_order) # 9 to NA
+table(natl2016_2020$nb_prenatal_visits, useNA = "always") # 99 to NA; character to numeric
+table(natl2016_2020$wic, useNA = "always") # U to NA; character to numeric
+
+table(natl2016_2020$nb_cig_before_preg, useNA = "always") # 99 to NA; character to numeric
+table(natl2016_2020$nb_cig_first_trimester, useNA = "always") # 99 to NA; character to numeric
+table(natl2016_2020$nb_cig_second_trimester, useNA = "always") # 99 to NA; character to numeric
+table(natl2016_2020$nb_cig_third_trimester, useNA = "always") # 99 to NA; character to numeric
+summary(natl2016_2020$bmi) # 99.90 to NA
+table(natl2016_2020$pre_preg_diabetes, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$gest_diabetes, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$pre_preg_htn, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$gest_htn, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$eclampsia, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$success_external_cephalic, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$fail_external_cephalic, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$labor_induction, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$labor_augmentation, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$labor_steroids, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$labor_antibiotics, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$labor_anesthesia, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$labor_chorioamniotitis, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$maternal_transfusion, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$ruptured_uterus, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$unplanned_hysterectomy, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$maternal_icu, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$payer, useNA = "always") # 9 to NA
+table(natl2016_2020$five_min_apgar, useNA = "always") # 99 to NA; character to numeric
+table(natl2016_2020$baby_sex, useNA = "always") # character to numeric
+table(natl2016_2020$gest_age, useNA = "always") # 99 to NA
+table(natl2016_2020$birthweight, useNA = "always") # 9999 to NA; character to numeric
+table(natl2016_2020$assisted_ventilation_six_hr, useNA = "always") # U to NA ; character to numeric
+table(natl2016_2020$baby_seizures, useNA = "always") # U to NA ; character to numeric
