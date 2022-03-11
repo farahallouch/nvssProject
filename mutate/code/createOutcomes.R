556d3b9fb065d411a3cd07b327c689b6f8ed48e4
Farah Allouch <farahmallouch@gmail.com>
2022-03-11 15:55:57 -0600

    adding exposure, outcomes, confounder categories + adding 5-min apgar to baby outcome index

--- mutate/code/createOutcomes.R
+++ mutate/code/createOutcomes.R
@@ -16,7 +16,8 @@ natl_gh <- natl_gh %>%
                                              maternal_icu == 1, 1, 0)),
          baby_outcomes = factor(ifelse(assisted_ventilation_six_hr == 1 |
                                          baby_seizures == 1 |
-                                         baby_nicu == 1, 1, 0)),
+                                         baby_nicu == 1 |
+                                         five_min_apgar < 7, 1, 0)),
          ptb = factor(ifelse(gest_age < 37, 1, 0)),
          lbw = factor(ifelse(birthweight < 2500, 1, 0)),
          sga = factor(ifelse((baby_sex == 0 & gest_age == 22 & birthweight < 375) | # male
@@ -64,6 +65,48 @@ natl_gh <- natl_gh %>%
                                (baby_sex == 1 & gest_age == 41 & birthweight < 2958) |
                                (baby_sex == 1 & gest_age == 42 & birthweight < 2985) |
                                (baby_sex == 1 & gest_age == 43 & birthweight < 2981) |
-                               (baby_sex == 1 & gest_age == 44 & birthweight < 2952), 1, 0)))
+                               (baby_sex == 1 & gest_age == 44 & birthweight < 2952), 1, 0)),
+         payer = case_when(payer == 1 ~ "Medicaid",
+                           payer == 2 ~ "Private insurance",
+                           payer == 3 ~ "Self-pay",
+                           payer == 4 ~ "Other"),
+         payer = factor(payer, levels = c("Private insurance", "Medicaid", "Self-pay", "Other")),
+         mom_nativity = case_when(mom_nativity == 1 ~ "Born in US",
+                                  mom_nativity == 2 ~ "Born outside US"),
+         mom_age_cat = case_when(mom_age < 20 ~ "< 20",
+                                 mom_age %in% (20 : 26) ~ "20-26",
+                                 mom_age %in% (27 : 34) ~ "27-34",
+                                 mom_age >= 35 ~ "35+"),
+         mom_educ_cat = case_when(mom_educ %in% (1 : 2) ~ "< HS",
+                                  mom_educ == 3 ~ "HS",
+                                  mom_educ == 4 ~ "Some college",
+                                  mom_educ %in% (5 : 8) ~ "College & above"),
+         mom_educ_cat = factor(mom_educ_cat, levels = c("< HS", "HS", "Some college", "College & above")),
+         mom_race_ethnicity_cat = case_when(mom_race_ethnicity == 1 ~ "NH white",
+                                            mom_race_ethnicity == 2 ~ "NH Black",
+                                            mom_race_ethnicity == 3 ~ "NH AIAN",
+                                            (mom_race_ethnicity == 4 | mom_race_ethnicity == 5) ~ "NH Asian/NHOPI",
+                                            mom_race_ethnicity == 6 ~ "NH 2+ races",
+                                            mom_race_ethnicity == 7 ~ "Hispanic"),
+         mom_race_ethnicity_cat = factor(mom_race_ethnicity_cat, levels = c("NH white", "NH Black", "NH AIAN", "NH Asian/NHOPI", "NH 2+ races", "Hispanic")),
+         preg_smoking = case_when((nb_cig_first_trimester == 0 &
+                                     nb_cig_second_trimester == 0 &
+                                     nb_cig_third_trimester == 0) ~ 0,
+                                  (nb_cig_first_trimester > 0 |
+                                     nb_cig_second_trimester > 0 |
+                                     nb_cig_third_trimester > 0) ~ 1),
+         prenatal_care = case_when((nb_prenatal_visits > 0 |
+                                      month_prenatal_start > 0 ) ~ 1,
+                                   (nb_prenatal_visits == 0 &
+                                      month_prenatal_start == 0) ~ 0,
+                                   (nb_prenatal_visits == 0 &
+                                      month_prenatal_start == NA) ~ 0,
+                                   (nb_prenatal_visits == NA &
+                                      month_prenatal_start == 0) ~ 0),
+         mom_marital_status = case_when(mom_marital_status == 1 ~ "Married",
+                                        mom_marital_status == 2 ~ "Unmarried"),
+         plurality = case_when(plurality == 1 ~ "1",
+                               plurality == 2 ~ "2",
+                               plurality >= 3 ~ "3+"))
 
 save(natl_gh, file = "mutate/output/natl_gh_outcomes.Rdata")
