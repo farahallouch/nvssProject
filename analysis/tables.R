d7c52f71d1df531531230b77152ba34eecf442f2
Farah Allouch <farahmallouch@gmail.com>
2022-04-05 08:53:52 -0500

    removing x -> m model from presenting mediation analysis

--- analysis/tables.R
+++ analysis/tables.R
@@ -71,7 +71,8 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_maternal_transfusion,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/maternal_transfusion.docx")
 
-rm(list = unadjusted_maternal_transfusion, adjusted_maternal_transfusion)
+rm(unadjusted_maternal_transfusion)
+rm(adjusted_maternal_transfusion)
 
 ## Ruptured uterus
 unadjusted_ruptured_uterus <- glm(ruptured_uterus ~ payer,
@@ -91,6 +92,9 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_ruptured_uterus,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/ruptured_uterus.docx")
 
+rm(unadjusted_ruptured_uterus)
+rm(adjusted_ruptured_uterus)
+
 ## Unplanned hysterectomy
 unadjusted_unplanned_hysterectomy <- glm(unplanned_hysterectomy ~ payer,
                                          data = natl_gh,
@@ -109,6 +113,9 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_unplanned_hysterectomy,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/unplanned_hysterectomy.docx")
 
+rm(unadjusted_unplanned_hysterectomy)
+rm(adjusted_unplanned_hysterectomy)
+
 ## Maternal ICU
 unadjusted_maternal_icu <- glm(maternal_icu ~ payer,
                                data = natl_gh,
@@ -126,6 +133,9 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_maternal_icu,
   modify_caption("Table 6. Maternal ICU") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/maternal_icu.docx")
+
+rm(unadjusted_maternal_icu)
+rm(adjusted_maternal_icu)
 
 ## Baby outcomes
 unadjusted_baby_outcomes <- glm(baby_outcomes ~ payer,
@@ -147,6 +157,8 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_baby_outcomes,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/baby_outcomes.docx")
 
+rm(unadjusted_baby_outcomes)
+
 ## Assisted ventilation > 6 hours
 unadjusted_assisted_vent <- glm(assisted_ventilation_six_hr ~ payer,
                                 data = natl_gh,
@@ -163,7 +175,10 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_assisted_vent,
           tab_spanner = c("**Unadjusted model**", "**Adjusted model**")) %>% 
   modify_caption("Table 8. Assisted ventilation > 6 hours") %>%
   as_flex_table() %>%
-  flextable::save_as_docx(path = "./analysis/output/maternal_outcomes.docx")
+  flextable::save_as_docx(path = "./analysis/output/assisted_ventilation.docx")
+
+rm(unadjusted_assisted_vent)
+rm(adjusted_assisted_vent)
 
 ## Infant Seizures
 unadjusted_baby_seizures <- glm(baby_seizures ~ payer,
@@ -183,6 +198,9 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_baby_seizures,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/baby_seizures.docx")
 
+rm(unadjusted_baby_seizures)
+rm(adjusted_baby_seizures)
+
 ## NICU
 unadjusted_baby_nicu <- glm(baby_nicu ~ payer,
                             data = natl_gh,
@@ -201,6 +219,9 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_baby_nicu,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/nicu.docx")
 
+rm(unadjusted_baby_nicu)
+rm(adjusted_baby_nicu)
+
 ## 5-min APGAR < 7 
 unadjusted_apgar_less_7 <- glm(apgar_less_7 ~ payer,
                                data = natl_gh,
@@ -218,6 +239,9 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_apgar_less_7,
   modify_caption("Table 11. Five minute APGAR score < 7") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/apgar_less_7.docx")
+
+rm(unadjusted_apgar_less_7)
+rm(adjusted_apgar_less_7)
 
 ## Preterm birth
 unadjusted_ptb <- glm(ptb ~ payer,
@@ -239,6 +263,8 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_ptb,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/preterm_birth.docx")
 
+rm(unadjusted_ptb)
+
 ## Low birthweight
 unadjusted_lbw <- glm(lbw ~ payer,
                       data = natl_gh,
@@ -258,6 +284,8 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_lbw,
   modify_caption("Table 13. Low birthweight") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/low_birthweight.docx")
+
+rm(unadjusted_lbw)
 
 ## Small for gestational age
 unadjusted_sga <- glm(sga ~ payer,
@@ -279,15 +307,17 @@ tbl_merge(tbls = list(tbl_regression(unadjusted_sga,
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/sga.docx")
 
+rm(unadjusted_sga)
+
 # Mediation analysis
 ## Maternal outcomes
 natl_gh <- natl_gh %>% 
   mutate(adequate_pnc = ifelse((apcnu == "Adequate Plus" |
                                   apcnu == "Adequate"), 1, 0))
 
-mediation_xm <- glm(adequate_pnc ~ payer + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
-                                      data = natl_gh,
-                                      family = binomial(link = "logit"))
+# mediation_xm <- glm(adequate_pnc ~ payer + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
+#                                       data = natl_gh,
+#                                       family = binomial(link = "logit"))
 
 mediation_maternal_outcomes_xmy <- glm(maternal_outcomes ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
                                        data = natl_gh,
@@ -295,16 +325,16 @@ mediation_maternal_outcomes_xmy <- glm(maternal_outcomes ~ payer + adequate_pnc 
 
 tbl_merge(tbls = list(tbl_regression(adjusted_maternal_outcomes,
                                      exponentiate = TRUE),
-                      tbl_regression(mediation_xm,
-                                     exponentiate = TRUE),
                       tbl_regression(mediation_maternal_outcomes_xmy,
                                      exponentiate = TRUE)),
-          tab_spanner = c("**Exposure outcome model**",
-                          "**Exposure mediator model**",
-                          "**Exposure mediator outcome model**")) %>% 
+          tab_spanner = c("**Exposure-outcome model**",
+                          "**Exposure, mediator-outcome model**")) %>% 
   modify_caption("Table 15. Maternal outcomes - mediation analysis") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/maternal_outcomes_mediation.docx")
+
+rm(adjusted_maternal_outcomes)
+rm(mediation_maternal_outcomes_xmy)
 
 ## Baby outcomes
 mediation_baby_outcomes_xmy <- glm(baby_outcomes ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
@@ -313,16 +343,16 @@ mediation_baby_outcomes_xmy <- glm(baby_outcomes ~ payer + adequate_pnc + mom_ag
 
 tbl_merge(tbls = list(tbl_regression(adjusted_baby_outcomes,
                                      exponentiate = TRUE),
-                      tbl_regression(mediation_xm,
-                                     exponentiate = TRUE),
                       tbl_regression(mediation_baby_outcomes_xmy,
                                      exponentiate = TRUE)),
-          tab_spanner = c("**Exposure outcome model**",
-                          "**Exposure mediator model**",
-                          "**Exposure mediator outcome model**")) %>% 
+          tab_spanner = c("**Exposure-outcome model**",
+                          "**Exposure, mediator-outcome model**")) %>% 
   modify_caption("Table 16. Baby outcomes - mediation analysis") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/baby_outcomes_mediation.docx")
+
+rm(adjusted_baby_outcomes)
+rm(mediation_baby_outcomes_xmy)
 
 ## Preterm birth
 mediation_ptb_xmy <- glm(ptb ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
@@ -331,16 +361,16 @@ mediation_ptb_xmy <- glm(ptb ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat
 
 tbl_merge(tbls = list(tbl_regression(adjusted_ptb,
                                      exponentiate = TRUE),
-                      tbl_regression(mediation_xm,
-                                     exponentiate = TRUE),
                       tbl_regression(mediation_ptb_xmy,
                                      exponentiate = TRUE)),
-          tab_spanner = c("**Exposure outcome model**",
-                          "**Exposure mediator model**",
-                          "**Exposure mediator outcome model**")) %>% 
+          tab_spanner = c("**Exposure-outcome model**",
+                          "**Exposure, mediator-outcome model**")) %>% 
   modify_caption("Table 17. Preterm birth - mediation analysis") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/preterm_birth_mediation.docx")
+
+rm(adjusted_ptb)
+rm(mediation_ptb_xmy)
 
 ## Low birthweight
 mediation_lbw_xmy <- glm(lbw ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
@@ -349,16 +379,16 @@ mediation_lbw_xmy <- glm(lbw ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat
 
 tbl_merge(tbls = list(tbl_regression(adjusted_lbw,
                                      exponentiate = TRUE),
-                      tbl_regression(mediation_xm,
-                                     exponentiate = TRUE),
                       tbl_regression(mediation_lbw_xmy,
                                      exponentiate = TRUE)),
-          tab_spanner = c("**Exposure outcome model**",
-                          "**Exposure mediator model**",
-                          "**Exposure mediator outcome model**")) %>% 
+          tab_spanner = c("**Exposure-outcome model**",
+                          "**Exposure, mediator-outcome model**")) %>% 
   modify_caption("Table 18. Low birthweight - mediation analysis") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/low_birthweight_mediation.docx")
+
+rm(adjusted_lbw)
+rm(mediation_lbw_xmy)
 
 ## Small for gestational age
 mediation_sga_xmy <- glm(sga ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat + mom_nativity + mom_race_ethnicity_cat,
@@ -367,13 +397,13 @@ mediation_sga_xmy <- glm(sga ~ payer + adequate_pnc + mom_age_cat + mom_educ_cat
 
 tbl_merge(tbls = list(tbl_regression(adjusted_sga,
                                      exponentiate = TRUE),
-                      tbl_regression(mediation_xm,
-                                     exponentiate = TRUE),
                       tbl_regression(mediation_sga_xmy,
                                      exponentiate = TRUE)),
-          tab_spanner = c("**Exposure outcome model**",
-                          "**Exposure mediator model**",
-                          "**Exposure mediator outcome model**")) %>% 
+          tab_spanner = c("**Exposure-outcome model**",
+                          "**Exposure, mediator-outcome model**")) %>% 
   modify_caption("Table 18. Small for gestational age - mediation analysis") %>%
   as_flex_table() %>%
   flextable::save_as_docx(path = "./analysis/output/sga_mediation.docx")
+
+rm(adjusted_sga)
+rm(mediation_sga_xmy)
